2480704fc55c16bdda70d1754f56af45c35e5964
Luciano Kolln <luciano.kolln@unmsm.edu.pe>
2023-07-01 18:00:40 -0500

    Iniciar Proyecto

---
+++ src/main/java/com/mycompany/ironcutpro/IronCutPro.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.ironcutpro;
+
+/**
+ *
+ * @author luciano
+ */
+public class IronCutPro {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
